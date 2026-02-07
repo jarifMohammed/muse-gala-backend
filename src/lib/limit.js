a93fa7d5004231058834e4ea8bb3ae8b02554f01
jarifMohammed <mohammedjarif@protonmail.com>
2026-02-07 14:26:46 +0600

    Increase global rate limit maximum to allow higher request volume

--- src/lib/limit.js
+++ src/lib/limit.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 const globalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 10000000,
 });
 
 const emailVerificationLimiter = rateLimit({
