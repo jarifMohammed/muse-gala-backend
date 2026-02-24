7c100f6642e1753df11c600cdfb726bea8b3db12
jarifMohammed <mohammedjarif@protonmail.com>
2026-02-24 11:09:38 +0600

    feat: remove lender stats routes file

--- src/entities/admin/overview/lenderStats.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { verifyToken, superAdminOrAdminMiddleware } from '../../../core/middlewares/authMiddleware.js';
-import { getLenderStats } from './lenderStats.controller.js';
-
-const router = express.Router();
-
-router.get('/lender-stats', verifyToken, superAdminOrAdminMiddleware, getLenderStats);
-
-export default router;
